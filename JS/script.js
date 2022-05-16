c4fcec0084d189c11aa82cb2813942c0a9018706
Marvin Virapin <marvin.virapin@hotmail.fr>
2022-05-16 09:54:03 +0200

    Ajout des fichier au dépôt

---
+++ JS/script.js
@@ -0,0 +1,16 @@
+
+document.querySelector(".titreBtn1").addEventListener("click", function () {
+    document.querySelector(".txtFaq1").classList.toggle("affiche");
+  });
+  document.querySelector(".titreBtn2").addEventListener("click", function () {
+    document.querySelector(".txtFaq2").classList.toggle("affiche");
+  });
+  document.querySelector(".titreBtn3").addEventListener("click", function () {
+    document.querySelector(".txtFaq3").classList.toggle("affiche");
+  });
+  document.querySelector(".titreBtn4").addEventListener("click", function () {
+    document.querySelector(".txtFaq4").classList.toggle("affiche");
+  });
+  document.querySelector(".titreBtn5").addEventListener("click", function () {
+    document.querySelector(".txtFaq5").classList.toggle("affiche");
+  });
